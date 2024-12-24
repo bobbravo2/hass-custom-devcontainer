9efe38136486754cc5ed79d456532c727cd7ccfa
Robert Gregor <348865+bobbravo2@users.noreply.github.com>
2024-12-23 19:49:17 -0500

    Update test.sh to change Docker image and comment out environment variable

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ docker run --rm -it \
     -p 5002:8123 \
     -v $(pwd):/workspaces/test \
     -v $(pwd):/config/www/workspace \
-    -e LOVELACE_PLUGINS="thomasloven/lovelace-card-mod thomasloven/lovelace-auto-entities custom-cards/button-card kalkih/mini-media-player" \
+    # -e LOVELACE_PLUGINS="thomasloven/lovelace-card-mod thomasloven/lovelace-auto-entities custom-cards/button-card kalkih/mini-media-player" \
     -e ENV_FILE="/workspaces/test/test.env" \
-    thomasloven/hass-custom-devcontainer sudo -E container run-dev
+    quay.io/bobbravo20/hass-custom-devcontainer sudo -E container run-dev
